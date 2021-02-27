633d02f8cd915bc9d12c597db2834211b19840a7
gabriuda <gabrielpr.martins@gmail.com>
2021-02-27 16:42:22 -0300

    Update menu-mobile.js

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,14 +1,13 @@
 export default function initMenuMobile() {
   const menuBotao = document.querySelector('[data-menu="botao"]');
   const menuLista = document.querySelector('[data-menu="lista"]');
-  const eventos = ['click', 'touchstart'];
 
   function openMenu() {
     menuBotao.classList.toggle('active');
     menuLista.classList.toggle('active');
   }
 
-  eventos.forEach(userEvent => menuBotao.addEventListener(userEvent, openMenu));
+  menuBotao.addEventListener('click', openMenu);
 }
 
 
