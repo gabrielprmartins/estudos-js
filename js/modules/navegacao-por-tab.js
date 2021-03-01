dd7e2fbaa6ce8516c1041c6ff5c360a26bda178d
gabriuda <gabrielpr.martins@gmail.com>
2021-02-28 22:25:11 -0300

    Update navegacao-por-tab.js

--- js/modules/navegacao-por-tab.js
+++ js/modules/navegacao-por-tab.js
@@ -1,21 +1,21 @@
 export default function initTabNavigation() {
+  const tabControls = document.querySelectorAll('[data-nav="controles"] li');
+  const tabContent = document.querySelectorAll('[data-nav="conteudo"] section');
+  if(tabControls && tabContent) {
+    tabControls[0].classList.add('ativo');
+    tabContent[0].classList.add('ativo');
 
+    function changeTab(index) {
+      tabContent.forEach(item => item.classList.remove('ativo'));
+      tabControls.forEach(item => item.classList.remove('ativo'));
+      tabContent[index].classList.add('ativo');
+      tabControls[index].classList.add('ativo');
+    }
+
+    tabControls.forEach((tab, index) => {
+      tab.addEventListener('click', () => {
+        changeTab(index);
+      });
+    })
+  }
 }
-
-const tabControls = document.querySelectorAll('[data-nav="controles"] li');
-const tabContent = document.querySelectorAll('[data-nav="conteudo"] section');
-tabControls[0].classList.add('ativo');
-tabContent[0].classList.add('ativo');
-
-function changeTab(index) {
-  tabContent.forEach(item => item.classList.remove('ativo'));
-  tabControls.forEach(item => item.classList.remove('ativo'));
-  tabContent[index].classList.add('ativo');
-  tabControls[index].classList.add('ativo');
-}
-
-tabControls.forEach((tab, index) => {
-  tab.addEventListener('click', () => {
-    changeTab(index);
-  });
-})
