7e1860d8f64670047d7754324668233aa161755e
gabriuda <gabrielpr.martins@gmail.com>
2021-03-03 16:37:09 -0300

    observer

--- js/modules/animation-numbers.js
+++ js/modules/animation-numbers.js
@@ -2,20 +2,31 @@ export default function initAnimationNumers() {
   const numbers = document.querySelectorAll('[data-numero]');
 
   if(numbers) {
-    numbers.forEach((n) => {
-      const numbersText = +n.innerText.replace('%','');
-      n.innerText = 0;
-      const intervalNum = setInterval(() => {
-        n.innerText++;
-        if(n.innerText >= numbersText) {
-          clearInterval(intervalNum);
-          n.innerText = numbersText + '%';
-        }
-      }, 20 * Math.random());
-      
-    });
+    function animaNumeros() {
+      numbers.forEach((n) => {
+        const numbersText = +n.innerText.replace('%','');
+        n.innerText = 0;
+        const intervalNum = setInterval(() => {
+          n.innerText++;
+          if(n.innerText >= numbersText) {
+            clearInterval(intervalNum);
+            n.innerText = numbersText + '%';
+          }
+        }, 20 * Math.random());
+      });
+    }
+
+    const observer = new MutationObserver(handleMutation);
+    const observerTarget = document.querySelector('.numeros-curso');
+    observer.observe(observerTarget, {attributes: true});
+
+    function handleMutation(mutation) {
+      if(mutation[0].target.classList.contains('anima-scroll')) {
+        observer.disconnect();
+        animaNumeros();
+      }
+    }
   }
 }
-
 
 
