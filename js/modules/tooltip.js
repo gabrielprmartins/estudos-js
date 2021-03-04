d8d927f6b8fb079eb67fe34c5aae26f7e68b7d96
gabriuda <gabrielpr.martins@gmail.com>
2021-03-04 02:03:18 -0300

    tooltip

---
+++ js/modules/tooltip.js
@@ -0,0 +1,46 @@
+export default function initTooltip() {
+  const tooltip = Array.from(document.querySelectorAll('[data-tooltip]'));
+
+  if(tooltip) {
+    function createTooltip(element) {
+      const tooltipBox = document.createElement('div');
+      const tooltipContent = element.getAttribute('aria-label');
+      tooltipBox.classList.add('tooltip');
+      tooltipBox.innerText = tooltipContent;
+      document.body.appendChild(tooltipBox);
+      return tooltipBox;
+    }
+  
+    const showTooltip = {
+      handleEvent(event) {
+        this.tooltipBox.style.top = event.pageY + 20 + 'px';
+        this.tooltipBox.style.left = event.pageX + 20 + 'px';
+      }
+    }
+  
+    const hiddenTooltip = {
+      handleEvent() {
+        this.tooltipBox.remove();
+        this.element.removeEventListener('mousemove', showTooltip); 
+        this.element.removeEventListener('mouseleave', hiddenTooltip);
+      }
+    }
+  
+    function handleTooltip() {
+      const tooltipBox = createTooltip(this);
+      const tDimensions = tooltipBox.getBoundingClientRect();
+      
+  
+      showTooltip.tooltipBox = tooltipBox;
+      showTooltip.tDimensions = tDimensions;
+      this.addEventListener('mousemove', showTooltip); 
+      
+      hiddenTooltip.element = this;
+      hiddenTooltip.tooltipBox = tooltipBox;
+      this.addEventListener('mouseleave', hiddenTooltip);
+    }
+  
+    tooltip.forEach(item => item.addEventListener('mouseover', handleTooltip));
+  }
+}
+
--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initTabNavigation from './modules/navegacao-por-tab.js';
 import initAnimationNumers from './modules/animation-numbers.js';
 import initAnimaScroll from './modules/anima-scroll.js';
+import initTooltip from './modules/tooltip.js';
 
 initMenuMobile();
 initTabNavigation();
 initAnimationNumers();
 initAnimaScroll();
+initTooltip();
