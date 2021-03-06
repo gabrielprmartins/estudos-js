cba595d5d1fa6332bb2fbeabb072c57570dc0344
gabriuda <gabrielpr.martins@gmail.com>
2021-03-06 03:18:00 -0300

    fetch-api

--- js/modules/chucknorris-api.js
+++ js/modules/chucknorris-api.js
@@ -1,27 +1,24 @@
 export default function initChuckNorrisJokes() {
+  const piadaParagrafo = document.querySelector('.piada-paragrafo');
+  const proximaPiada = document.querySelector('.btn-proximo');
+  const loading = document.querySelector('.loading');
 
+  if(!piadaParagrafo.innerText.length) {
+    loading.classList.add('active');
+    piadaParagrafo.appendChild(loading);
+  }
+
+  function puxarPiada() {
+    piadaParagrafo.innerText = '';
+    loading.classList.add('active');
+    piadaParagrafo.appendChild(loading);
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(r => r.json()
+    .then(piada => {
+      piadaParagrafo.innerText = piada.value;
+    }));
+  }
+
+  proximaPiada.addEventListener('click', puxarPiada);
+  puxarPiada();
 }
-
-const piadaParagrafo = document.querySelector('.piada-paragrafo');
-const proximaPiada = document.querySelector('.btn-proximo');
-const loading = document.querySelector('.loading');
-
-if(!piadaParagrafo.innerText.length) {
-  loading.classList.add('active');
-  piadaParagrafo.appendChild(loading);
-}
-
-
-function puxarPiada() {
-  piadaParagrafo.innerText = '';
-  loading.classList.add('active');
-  piadaParagrafo.appendChild(loading);
-  fetch('https://api.chucknorris.io/jokes/random')
-  .then(r => r.json()
-  .then(piada => {
-    piadaParagrafo.innerText = piada.value;
-  }));
-}
-
-proximaPiada.addEventListener('click', puxarPiada);
-puxarPiada();
