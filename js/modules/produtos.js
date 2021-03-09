babff0e4dd35efbbb458cdfa75f233c34073149d
gabriuda <gabrielpr.martins@gmail.com>
2021-03-09 11:59:46 -0300

    produtos-info

--- js/modules/produtos.js
+++ js/modules/produtos.js
@@ -17,21 +17,60 @@ export function initProdutos() {
   }
   
   function getProdutos(produtos) {
-    const produtosDom = document.querySelector('[data-produtos]');
+    const produtosGrid = document.querySelector('[data-produtos]');
     
-    if(produtosDom) {
+    if(produtosGrid) {
       produtos.forEach(produto => {
-        produtosDom.innerHTML += `
+        produtosGrid.innerHTML += `
           <div class="produto">
             <img src="${produto.fotos[0].src}" alt="${produto.fotos[0].titulo}" class="produto-img">
             <div class="info-produto">
+              <span class="preco-produto">R$ ${produto.preco}</span>
               <h2 class="nome-produto">${produto.nome}</h2>
-              <span class="preco-produto">${produto.preco}</span>
             </div>
           </div>
         `;
       })
+
+      const produtosDiv = document.querySelectorAll('.produto');
+      if(produtosDiv) produtosDiv.forEach(p => p.addEventListener('click', (e) => hiperlinkProducts(e, produtos)));
     }
+  }
+
+  function hiperlinkProducts(e, produtos) {
+    const produtosGrid = document.querySelector('[data-produtos]');
+    
+    produtos.forEach(produto => {
+      document.body.scrollIntoView();
+      if(produto.fotos[0].src === e.target.src) {
+        // window.history.pushState(null, null, 'produto/'+ produto.nome);
+        produtosGrid.innerHTML = `
+          <div class="produto-especificacoes">
+           <button class="voltar-produto" data-produto="voltar">◄ Voltar</button>
+            <img src="${produto.fotos[0].src}" alt="${produto.fotos[0].titulo}" class="produto-img">
+            <div class="especificacoes">
+              <h2 class="nome-produto">${produto.nome}</h2>
+              <span class="preco-produto">R$ ${produto.preco}</span>
+              <p class="paragrafo">${produto.descricao}</p>
+            </div>
+          </div>
+        `;
+        document.title = document.querySelector('title').innerText + ' | ' + produto.nome;
+        const btnVoltarProdutos = document.querySelector('[data-produto="voltar"]');
+        const produtoEspecificacoes = document.querySelector('.produto-especificacoes');
+
+        if(btnVoltarProdutos && produtoEspecificacoes) btnVoltarProdutos.addEventListener('click', () => {
+          produtoEspecificacoes.remove();
+          getProdutos(produtos);
+        })
+        if(document.title.includes(produto.nome)) {
+          window.addEventListener('popstate', () => { 
+            getProdutos(produtos);
+            produtoEspecificacoes.remove();
+          });
+        } 
+      }
+    })
   }
   
   fetchProdutos('https://ranekapi.origamid.dev/json/api/produto');
