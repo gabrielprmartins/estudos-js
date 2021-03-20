c20c5a9a00893c2510d6be1a190d1c15d8ca0732
Gabriel Pereira <gabrielpr.martins@gmail.com>
2021-03-19 21:19:03 -0300

    tooltip refatorado com classe

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,42 +1,46 @@
-export default function initTooltip() {
-  const tooltip = Array.from(document.querySelectorAll('[data-tooltip]'));
+export default class Tooltip {
+  constructor(tooltips) {
+    this.tooltip = Array.from(document.querySelectorAll(tooltips));
 
-  if (tooltip) {
-    function createTooltip(element) {
-      const tooltipBox = document.createElement('div');
-      const tooltipContent = element.getAttribute('aria-label');
-      tooltipBox.classList.add('tooltip');
-      tooltipBox.innerText = tooltipContent;
-      document.body.appendChild(tooltipBox);
-      return tooltipBox;
+    this.showTooltip = this.showTooltip.bind(this);
+    this.hiddenTooltip = this.hiddenTooltip.bind(this);
+    this.handleTooltip = this.handleTooltip.bind(this);
+  }
+
+  showTooltip(event) {
+    this.tooltipBox.style.top = `${event.pageY + 20}px`;
+    this.tooltipBox.style.left = `${event.pageX + 20}px`;
+  }
+
+  hiddenTooltip({ currentTarget }) {
+    this.tooltipBox.remove();
+    currentTarget.removeEventListener('mouseleave', this.hiddenTooltip);
+    currentTarget.removeEventListener('mousemove', this.showTooltip);
+  }
+
+  createTooltip(element) {
+    const tooltipBox = document.createElement('div');
+    const tooltipContent = element.getAttribute('aria-label');
+    tooltipBox.classList.add('tooltip');
+    tooltipBox.innerText = tooltipContent;
+    document.body.appendChild(tooltipBox);
+    this.tooltipBox = tooltipBox;
+  }
+
+  handleTooltip({ currentTarget }) {
+    this.createTooltip(currentTarget);
+    currentTarget.addEventListener('mousemove', this.showTooltip);
+    currentTarget.addEventListener('mouseleave', this.hiddenTooltip);
+  }
+
+  addTooltipEvent() {
+    this.tooltip.forEach((item) => item.addEventListener('mouseover', this.handleTooltip));
+  }
+
+  init() {
+    if (this.tooltip.length) {
+      this.addTooltipEvent();
     }
-
-    const showTooltip = {
-      handleEvent(event) {
-        this.tooltipBox.style.top = `${event.pageY + 20}px`;
-        this.tooltipBox.style.left = `${event.pageX + 20}px`;
-      },
-    };
-
-    const hiddenTooltip = {
-      handleEvent() {
-        this.tooltipBox.remove();
-        this.element.removeEventListener('mousemove', showTooltip);
-        this.element.removeEventListener('mouseleave', hiddenTooltip);
-      },
-    };
-
-    function handleTooltip() {
-      const tooltipBox = createTooltip(this);
-
-      showTooltip.tooltipBox = tooltipBox;
-      this.addEventListener('mousemove', showTooltip);
-
-      hiddenTooltip.element = this;
-      hiddenTooltip.tooltipBox = tooltipBox;
-      this.addEventListener('mouseleave', hiddenTooltip);
-    }
-
-    tooltip.forEach((item) => item.addEventListener('mouseover', handleTooltip));
+    return this;
   }
 }
