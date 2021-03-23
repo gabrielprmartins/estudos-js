ad2a18d217126700420f6a2f74d64e10d90b37d6
Gabriel Pereira <gabrielpr.martins@gmail.com>
2021-03-23 01:06:09 -0300

    debounce adicionado

--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -1,10 +1,12 @@
+import debounce from './debounce.js';
+
 export default class AnimaScroll {
   constructor(sections) {
     this.sections = Array.from(document.querySelectorAll(sections));
     this.windowMetade = window.innerHeight * 0.7;
     this.activeClass = 'anima-scroll';
 
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
 
   getDistance() {
