584fc7344639ca9612e1b4a1d56476782d5df565
Gabriel Pereira <gabrielpr.martins@gmail.com>
2021-03-23 00:55:42 -0300

    anima scroll refatorado com classe

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 import MenuMobile from './modules/menu-mobile.js';
 import TabNavigation from './modules/navegacao-por-tab.js';
 import AnimationNumers from './modules/animation-numbers.js';
-import initAnimaScroll from './modules/anima-scroll.js';
 import Tooltip from './modules/tooltip.js';
 import Writer from './modules/writer.js';
 import initHistoryApi from './modules/historyapi.js';
+import AnimaScroll from './modules/anima-scroll.js';
 
 const menuMobile = new MenuMobile('[data-menu="botao"]', '[data-menu="lista"]');
 menuMobile.init();
@@ -15,7 +15,8 @@ tabNav.init();
 const animationNumbers = new AnimationNumers('[data-numero]', 'anima-scroll', '.numeros-curso');
 animationNumbers.init();
 
-initAnimaScroll();
+const animaScroll = new AnimaScroll('[data-scroll]');
+animaScroll.init();
 
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
@@ -26,9 +27,10 @@ writers.init();
 initHistoryApi();
 
 export function activeFunctions() {
+  const animaScrollActive = new AnimaScroll('[data-scroll]');
+  animaScrollActive.init();
   const tabNavActive = new TabNavigation('[data-nav="controles"] li button', '[data-nav="conteudo"] section');
   tabNavActive.init();
-  initAnimaScroll();
   const tooltipActive = new Tooltip('[data-tooltip]');
   tooltipActive.init();
   const writersActive = new Writer('[data-writer]');
