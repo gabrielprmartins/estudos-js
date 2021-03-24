9e9706f053ab95d25dad67eafb54377dab4cbd0a
Gabriel Pereira <gabrielpr.martins@gmail.com>
2021-03-24 18:40:01 -0300

    produtos refatorado

--- js/modules/produtos.js
+++ js/modules/produtos.js
@@ -1,6 +1,6 @@
 import loading from './loading.js';
 import erro from './error.js';
-import activeFunctions from './active-functions.js';
+import activeFunctions from './active-functions';
 
 export function initProdutos() {
   async function fetchProdutos(url) {
@@ -18,22 +18,8 @@ export function initProdutos() {
   }
 
   function getProdutos(produtos) {
-    const tituloProdutos = document.createElement('h1');
-    tituloProdutos.className = 'titulo-principal produto-titulo';
-    tituloProdutos.innerText = 'Produtos';
-    tituloProdutos.setAttribute('data-writer', '');
-
-    const produtosGrid = document.querySelector('[data-produtos]');
-    if (produtosGrid) produtosGrid.classList.add('get-in');
-
-    const tituloPrincipal = document.querySelector('.titulo-principal');
-    if (!tituloPrincipal) {
-      if (produtosGrid) {
-        produtosGrid.parentElement.insertBefore(tituloProdutos, produtosGrid);
-      }
-    }
     activeFunctions();
-
+    const produtosGrid = document.querySelector('[data-produtos]');
     if (produtosGrid) {
       produtos.forEach((produto) => {
         produtosGrid.innerHTML += `
@@ -46,48 +32,8 @@ export function initProdutos() {
           </div>
         `;
       });
-
-      const produtosDiv = document.querySelectorAll('.produto');
-      if (produtosDiv) produtosDiv.forEach((p) => p.addEventListener('click', (e) => permalinkProducts(e, produtos)));
     }
   }
 
-  function permalinkProducts(e, produtos) {
-    const produtosGrid = document.querySelector('[data-produtos]');
-    produtosGrid.classList.remove('get-in');
-    const tituloPrincipal = document.querySelector('.titulo-principal');
-    const produtoId = document.querySelectorAll('[data-produto-id]');
-
-    produtos.forEach((produto, i) => {
-      if (e.currentTarget === produtoId[i]) {
-        if (tituloPrincipal) tituloPrincipal.remove();
-        document.body.scrollIntoView();
-        // window.history.pushState(null, null, 'produto/'+ produto.nome);
-        produtosGrid.innerHTML = `
-          <div class="produto-especificacoes">
-           <button class="voltar-produto" data-produto="voltar"><div>➜</div> Voltar</button>
-            <img src="${produto.fotos[0].src}" alt="${produto.fotos[0].titulo}" class="produto-espec-img">
-            <div class="especificacoes">
-              <h2 class="nome-produto">${produto.nome}</h2>
-              <span class="preco-produto">R$ ${produto.preco}</span>
-              <p class="paragrafo">${produto.descricao}</p>
-            </div>
-          </div>
-        `;
-
-        const btnVoltarProdutos = document.querySelector('[data-produto="voltar"]');
-        const produtoEspecificacoes = document.querySelector('.produto-especificacoes');
-
-        if (btnVoltarProdutos && produtoEspecificacoes) {
-          btnVoltarProdutos.addEventListener('click', () => {
-            produtoEspecificacoes.remove();
-            produtosGrid.classList.add('get-in');
-            getProdutos(produtos);
-          });
-        }
-      }
-    });
-  }
-
-  fetchProdutos('https://ranekapi.origamid.dev/json/api/produto');
+  return fetchProdutos('https://ranekapi.origamid.dev/json/api/produto');
 }
