328afaace5cc4ee9f055abdd10c3b7c7e4a3cdaa
Gabriel Pereira <gabrielpr.martins@gmail.com>
2021-03-30 19:03:25 -0300

    contador funcionando

--- js/modules/contador.js
+++ js/modules/contador.js
@@ -1,7 +1,14 @@
 export default function contador() {
-  const funcionamento = document.querySelector('[data-semana]');
-  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+  const operation = document.querySelector('[data-semana]');
+  const activeClass = 'aberto';
+
+  const dataOperating = () => ({
+    diasSemana: operation.dataset.semana.split(',').map(Number),
+    horarioSemana: operation.dataset.horario.split(',').map(Number),
+  });
+
+  let dataOp;
+  if (operation) dataOp = dataOperating();
 
   const getActualTime = () => {
     const dataAtual = new Date();
@@ -16,32 +23,52 @@ export default function contador() {
   const getTimeToOpen = () => {
     const time = getActualTime();
     return {
-      hora: (horarioSemana[0] - time.horarioAtual) - 1,
+      hora: ((dataOp.horarioSemana[0] - time.horarioAtual) - 1),
       minuto: 59 - time.minutoAtual,
       segundo: 59 - time.segundoAtual,
     };
   };
 
+  const formatTime = () => {
+    const open = getTimeToOpen();
+    let { hora, minuto, segundo } = open;
+    if (hora < 0) hora += 24;
+    hora = (hora < 10) ? `0${hora}` : hora;
+    minuto = (minuto < 10) ? `0${minuto}` : minuto;
+    segundo = (segundo < 10) ? `0${segundo}` : segundo;
+
+    return {
+      hora,
+      minuto,
+      segundo,
+    };
+  };
+
   const initCountDown = () => {
-    funcionamento.classList.remove('aberto');
+    operation.classList.remove(activeClass);
     const count = setInterval(() => {
-      const open = getTimeToOpen();
-      funcionamento.innerText = `${open.hora}:${open.minuto}:${open.segundo}`;
+      const format = formatTime();
+      operation.innerText = `${format.hora}:${format.minuto}:${format.segundo}`;
+      const isTimeToOpen = (format.hora === '00') && (format.minuto === '00') && (format.segundo === '00');
+      if (isTimeToOpen) {
+        open();
+        clearInterval(count);
+      }
     }, 1000);
 
     return count;
   };
 
   const open = () => {
-    funcionamento.classList.add('aberto');
-    funcionamento.innerText = 'Aberto';
+    operation.classList.add(activeClass);
+    operation.innerText = 'Aberto';
   };
 
   const onDayMatches = () => {
     const time = getActualTime();
-    const semanaAberto = diasSemana.indexOf(time.diaAtual) !== -1;
-    const horarioAberto = (time.horarioAtual >= horarioSemana[0]
-      && time.horarioAtual < horarioSemana[1]);
+    const semanaAberto = dataOp.diasSemana.indexOf(time.diaAtual) !== -1;
+    const horarioAberto = (time.horarioAtual >= dataOp.horarioSemana[0]
+      && time.horarioAtual < dataOp.horarioSemana[1]);
 
     const countdown = initCountDown();
     if (semanaAberto && horarioAberto) {
@@ -50,7 +77,7 @@ export default function contador() {
     }
   };
 
-  if (funcionamento && diasSemana && horarioSemana) {
+  if (operation && dataOp.diasSemana && dataOp.horarioSemana) {
     onDayMatches();
   }
 }
